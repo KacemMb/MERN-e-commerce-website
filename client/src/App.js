18d2cf5a69e15286fbc2d51fed47d91210e768e8
KacemMb <mabroukikacem10001@gmail.com>
2024-07-17 12:00:12 +0100

    fix the product form logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import AdminDashboard from './Pages/AdminDashboard';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div className="App">
+      <div><Toaster/></div>
       <AdminDashboard />
     </div>
   );
