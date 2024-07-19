3fcdf65979617855e79e4db0ee06986dc3022951
GhiathBr <ghiathbrai46@gmail.com>
2024-07-19 10:22:11 +0100

    modify server.js to use separate DB connection file

--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
 import Product_route from './Routes/Product.route.js';
 import UserRoute from './Routes/User.route.js';
 import cors from 'cors';
+import connectDB from './DB/Connection.js';
 
 dotenv.config();
 const app = express();
@@ -11,11 +11,7 @@ app.use(cors());
 app.use(express.json())
 const port = process.env.PORT || 3000
 //connect to db
-const conn = mongoose.connection;
-mongoose.connect(process.env.MONGO);
-conn.once('open', () => {
-  console.log('connected to the database');
-})
+connectDB()
 app.use('/api/product',Product_route)
 app.use('/api/user',UserRoute)
 app.listen(port, () => {
