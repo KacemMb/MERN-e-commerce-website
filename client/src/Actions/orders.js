7f34b7e7bdff3461090cfd1a1208668e6f813e51
Eya-ZNEIDIA <zneidiaeya@gmail.com>
2024-07-28 13:29:14 +0100

    fonctionality of products,users and orders

---
+++ client/src/Actions/orders.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+export const FETCH_ORDERS_REQUEST = 'FETCH_ORDERS_REQUEST';
+export const FETCH_ORDERS_SUCCESS = 'FETCH_ORDERS_SUCCESS';
+export const FETCH_ORDERS_FAILURE = 'FETCH_ORDERS_FAILURE';
+
+export const fetchOrdersRequest = () => ({
+  type: FETCH_ORDERS_REQUEST
+});
+
+export const fetchOrdersSuccess = (orders) => ({
+  type: FETCH_ORDERS_SUCCESS,
+  payload: orders
+});
+
+export const fetchOrdersFailure = (error) => ({
+  type: FETCH_ORDERS_FAILURE,
+  payload: error
+});
+
+export const fetchOrders = () => {
+  return (dispatch) => {
+    dispatch(fetchOrdersRequest());
+    axios.get('http://localhost:2024/api/orders')
+      .then(response => {
+        dispatch(fetchOrdersSuccess(response.data));
+      })
+      .catch(error => {
+        dispatch(fetchOrdersFailure(error.message));
+      });
+  };
+};
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
+import { Provider } from 'react-redux'
+import store from './Store';
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
 );
