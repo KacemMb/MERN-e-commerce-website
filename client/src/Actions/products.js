ee9ee3c38316ff30c23ec24fd8b58dea8946faa9
Eya-ZNEIDIA <zneidiaeya@gmail.com>
2024-07-28 16:43:40 +0100

    correcting the fonctionality of products ?

--- client/src/Actions/products.js
+++ client/src/Actions/products.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
+
 // Action Types
 export const FETCH_PRODUCTS_REQUEST = 'FETCH_PRODUCTS_REQUEST';
 export const FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS';
 export const FETCH_PRODUCTS_FAILURE = 'FETCH_PRODUCTS_FAILURE';
-
 
 // Action Creators
 export const fetchProductsRequest = () => ({
@@ -20,17 +20,15 @@ export const fetchProductsFailure = (error) => ({
   payload: error
 });
 
-
 // Thunk Actions
 export const fetchProducts = () => {
   return async dispatch => {
     dispatch(fetchProductsRequest());
     try {
-      const response = await axios.get('http://localhost:2024/api/product/GetAllProducts')
+      const response = await axios.get('http://localhost:2024/api/product/GetAllProducts');
       dispatch(fetchProductsSuccess(response.data));
     } catch (error) {
       dispatch(fetchProductsFailure(error.message));
     }
   };
 };
-
--- client/src/Components/Products.jsx
+++ client/src/Components/Products.jsx
@@ -1,58 +1,56 @@
-import React, { useEffect }  from 'react';
+import React, { useEffect } from 'react';
 import '../Styles/Products.css';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchProducts } from '../Actions/products';
+
 const Products = ({ productsData, fetchProducts }) => {
   useEffect(() => {
     fetchProducts();
   }, [fetchProducts]);
-  const getUsername = (productId) => {
-    const product = productsData.products.find(product => product._id === productId);
-    return product && product.user ? product.user.full_name : '';
-  };
   
   if (productsData.loading) return <div>Loading...</div>;
   if (productsData.error) return <div>Error: {productsData.error}</div>;
+
   return (
-  <div className='Products'>
-    <table className="product-table">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Price</th>
-          <th>Sold</th>
-          <th>Quantity</th>
-          <th>Origin</th>
-          <th>Category</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {productsData.map((product) => (
-          <tr key={product.id}>
-            <td>{product.id}</td>
-            <td>{product.name}</td>
-            <td>{product.price}</td>
-            <td>{product.sold}</td>
-            <td>{product.quantity}</td>
-            <td>{product.origin}</td>
-            <td>{product.category}</td>
-            <td>
-               <Link to={`/admin/update`}>
+    <div className='Products'>
+      <table className="product-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Sold</th>
+            <th>Quantity</th>
+            <th>Origin</th>
+            <th>Category</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {productsData.products.map((product) => (
+            <tr key={product._id}>
+              <td>{product._id}</td>
+              <td>{product.name}</td>
+              <td>{product.price}</td>
+              <td>{product.sold}</td>
+              <td>{product.quantity}</td>
+              <td>{product.origin}</td>
+              <td>{product.category}</td>
+              <td>
+                <Link to={`/admin/update/${product._id}`}>
                   <button className="modify-button">Modify</button>
                 </Link>
-              <button className="delete-button">Delete</button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-
-   </div>
-   );  
+                <button className="delete-button">Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
+
 const mapStateToProps = state => ({
   productsData: state.products
 });
